15432e5888ad4d846d731fac9b8503d82817a2ce
ThobiasKH <thobiashoivik@gmail.com>
2024-10-17 13:42:02 +0200

    Separate kernel and bootloader

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,56 +1,45 @@
-org 0x7C00
+org 0x0
 bits 16
 
-%define ENDL 0x0D, 0x0A 
+
+%define ENDL 0x0D, 0x0A
+
 
 start:
-    jmp main
+    ; print hello world message
+    mov si, msg_hello
+    call puts
 
-; print function
-; Parameters: 
-;   - ds:si points to starting memory adress of string 
+.halt:
+    cli
+    hlt
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
 puts:
+    ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb         ; loads next character in al
-    or al, al     ; verify if next character is null (meaning the end of the string)
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0e  ; call bios interrupt
-    mov bh, 0
-    int 0x10      ; interrupt #10
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
 
     jmp .loop
 
 .done:
+    pop bx
     pop ax
-    pop si
-    ret    
+    pop si    
+    ret
 
-
-main:
-    ; set up data segments 
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-
-    ; set up stack
-    mov ss, ax
-    mov sp, 0x7C00 ; Stack grows downward from where we are loaded in memory so it doesn't overwrite the OS
-
-    ; print message
-    mov si, msg_hello
-    call puts
-
-    hlt
-
-.halt:
-    jmp .halt
-
-
-msg_hello: db 'Hello World', ENDL, 0
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Kernel says hello!', ENDL, 0
