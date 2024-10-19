f9bc4259dda99453c4735942f83bd6dd8c9d9bf4
ThobiasKH <thobiashoivik@gmail.com>
2024-10-19 15:01:48 +0200

    Multistage bootloader

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "x86.h"
 
-void putc(char c) { x86_VideoWriteCharTeletype(c, 0); }
+void putc(char c) { x86_Video_WriteCharTeletype(c, 0); }
 
 void puts(const char *str) {
     while (*str) {
--- src/bootloader/stage2/x86.h
+++ src/bootloader/stage2/x86.h
@@ -1,4 +1,4 @@
 #pragma once
 #include "stdint.h"
 
-void _cdecl _x86_VideoWriteCharTeletype(char c, uint8_t page);
+void _cdecl x86_Video_WriteCharTeletype(char c, uint8_t page);
